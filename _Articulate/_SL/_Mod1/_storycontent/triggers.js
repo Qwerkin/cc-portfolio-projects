d99249d57abb6cc760a37dc666e7d19dc4f33e00
Qwerkin <carolyn.ux@gmail.com>
2025-03-16 15:34:22 -0500

    Add files via upload

---
+++ _Articulate/_SL/_Mod1/_storycontent/triggers.js
@@ -0,0 +1,42 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6AJ4olkIiFl":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6SLABMzOYs2');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '6jg6LxBTVAg';
+const shakeAmount = 5;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
